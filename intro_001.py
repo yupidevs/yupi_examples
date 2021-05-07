c87e3484e011e5e047919a711fe687b365b44529
Gustavo Viera López <gvieralopez@gmail.com>
2021-05-07 17:47:34 +0200

    Update intro_001.py to comply with yupi v0.4

--- intro_001.py
+++ intro_001.py
@@ -6,7 +6,7 @@ x = [0, 1.0, 0.63, -0.37, -1.24, -1.5, -1.08, -0.19, 0.82, 1.63, 1.99, 1.85]
 y = [0, 0, 0.98, 1.24, 0.69, -0.3, -1.23, -1.72, -1.63, -1.01, -0.06, 0.94]
 
 # Generate the trajectory object
-track = Trajectory(x=x, y=y, id="Spiral")
+track = Trajectory(x=x, y=y, traj_id="Spiral")
 
 # Plot the results
 plot_trajectories([track])
