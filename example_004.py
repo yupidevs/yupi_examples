c2d856b955d41fe71bd19ac647049809a55e8973
Gustavo Viera López <gvieralopez@gmail.com>
2021-06-26 16:48:24 +0200

    Refactor example 004

--- example_004.py
+++ example_004.py
@@ -1,9 +1,43 @@
+"""
+Example 4:
+
+Tracking an intruder while penetrating a granular 
+material in a quasi 2D enviroment.
+
+The work carried out in [1] studied 
+penetration of an intruder inside a granular material, 
+focusing in the influence of a wall for the trajectory 
+of the intruder. The authors tested different configurations 
+and observed specific phenomenons during the penetration 
+process (i.e. repulsion and rotation), enabling the 
+quantification of those phenomenons and its dependence 
+with the initial distance of the intruder from the wall.
+
+In this example, we provide a script that extracts the 
+trajectory of the intruder from one of the videos used 
+to produce the results of the original paper. Moreover, 
+we include details to generate a plot that looks like the 
+one presented in the paper.
+
+A detailed explanation of this code can be found in:
+https://yupi.readthedocs.io/en/latest/examples/example4.html
+
+
+References
+----------
+[1] Díaz-Melián, V. L., et al. "Rolling away from the Wall 
+into Granular Matter." Physical Review Letters 125.7 (2020): 
+078002.
+
+"""
+# Import dependencies
 from yupi.tracking.trackers import ROI, ObjectTracker, TrackingScenario
 from yupi.tracking.undistorters import RemapUndistorter
 from yupi.tracking.algorithms import ColorMatching
 from yupi.analyzing.visualization import plot_trajectories
 from numpy import pi
 
+# Specify path to the required resources
 video_path = 'resources/videos/Diaz2020.MP4'
 camera_file = 'resources/cameras/gph3+.npz'
 
@@ -29,8 +63,8 @@ plot_trajectories(tl)
 
 
 # Rotate trajectories to be consistent with gravity  
-tl[0].add_polar_offset(0, - pi / 2)
-tl[1].add_polar_offset(0, - pi / 2)
+tl[0].rotate(- pi / 2)
+tl[1].rotate(- pi / 2)
 
 # Center trajectories respect the center of the intruder
 off = tl[0].r[0]
