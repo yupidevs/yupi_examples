aea4bd79ebe16cf49ab54556747158c164359c8f
Gustavo Viera López <gvieralopez@gmail.com>
2021-08-06 22:15:13 +0200

    Fix intro_004

--- intro_004.py
+++ intro_004.py
@@ -23,33 +23,33 @@ traj = trajs[0]
 
 
 # Spacial trajectories
-# yupi.graphics.plot_2D(trajs[:10], legend=False)
+yupi.graphics.plot_2D(trajs[:10], legend=False)
 
 # Filter
-# traj.traj_id = 'non filtered'
-# smoothed_traj = exp_convolutional_filter(traj, 1, 'filtered')
-# yupi.graphics.plot_2D([traj, smoothed_traj], legend=True)
+traj.traj_id = 'non filtered'
+smoothed_traj = exp_convolutional_filter(traj, 1, 'filtered')
+yupi.graphics.plot_2D([traj, smoothed_traj], legend=True)
 
-# # Velocity histogram
-# v = yupi.stats.speed_ensemble(trajs, step=1)
-# yupi.graphics.plot_velocity_hist(v, bins=20)
+# Velocity histogram
+v = yupi.stats.speed_ensemble(trajs, step=1)
+yupi.graphics.plot_velocity_hist(v, bins=20)
 
-# # Turning angles
-# theta = yupi.stats.turning_angles_ensemble(trajs)
-# yupi.graphics.plot_angles_hist(theta)
+# Turning angles
+theta = yupi.stats.turning_angles_ensemble(trajs)
+yupi.graphics.plot_angles_hist(theta)
 
-# # Mean square displacement
-# msd, msd_std = yupi.stats.msd(trajs, time_avg=True, lag=30)
-# yupi.graphics.plot_msd(msd, msd_std, dt, lag=30)
+# Mean square displacement
+msd, msd_std = yupi.stats.msd(trajs, time_avg=True, lag=30)
+yupi.graphics.plot_msd(msd, msd_std, dt, lag=30)
 
 # Kurtosis
-# kurtosis = yupi.stats.kurtosis(trajs, time_avg=False, lag=30)
-# kurt_ref = yupi.stats.kurtosis_reference(trajs)
-# yupi.graphics.plot_kurtosis(kurtosis, kurtosis_ref=kurt_ref, dt=dt)
+kurtosis = yupi.stats.kurtosis(trajs, time_avg=False, lag=30)
+kurt_ref = yupi.stats.kurtosis_reference(trajs)
+yupi.graphics.plot_kurtosis(kurtosis, kurtosis_ref=kurt_ref, dt=dt)
 
-# # Velocity autocorrelation function
-# vacf, _ = yupi.stats.vacf(trajs, time_avg=True, lag=50)
-# yupi.graphics.plot_vacf(vacf, dt, 50)
+# Velocity autocorrelation function
+vacf, _ = yupi.stats.vacf(trajs, time_avg=True, lag=50)
+yupi.graphics.plot_vacf(vacf, dt, 50)
 
 # Power Spectral Density
 psd_mean, psd_std, omega = yupi.stats.psd(trajs, lag=200, omega=True)
