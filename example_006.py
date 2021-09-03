1a46eba53a82fdad0c0d528c8cfe433a15731929
Gustavo Viera López <gvieralopez@gmail.com>
2021-09-03 22:35:36 +0200

    Update example 006

--- example_006.py
+++ example_006.py
@@ -1,35 +1,34 @@
 """
 Example 6
 
-A model framework of a diffusion process with fluctuating diffusivity 
-is presented. A Brownian but non-Gaussian diffusion by means of a coupled 
-set of stochastic differential equations is predicted. Position is 
-described by an overdamped Langevin equation and the diffusion coefficient 
+A model framework of a diffusion process with fluctuating diffusivity
+is presented. A Brownian but non-Gaussian diffusion by means of a coupled
+set of stochastic differential equations is predicted. Position is
+described by an overdamped Langevin equation and the diffusion coefficient
 as the square of an Ornstein-Uhlenbeck process.
 
-The example is focused in computing the probability density function for 
-displacements at different time instants for the case of a one-dimensional 
+The example is focused in computing the probability density function for
+displacements at different time instants for the case of a one-dimensional
 process, as shown analitically by Chechkin et al. in [1].
 
 
 References
 ----------
-[1] Chechkin, Aleksei V., et al. "Brownian yet non-Gaussian diffusion: 
-from superstatistics to subordination of diffusing diffusivities." 
+[1] Chechkin, Aleksei V., et al. "Brownian yet non-Gaussian diffusion:
+from superstatistics to subordination of diffusing diffusivities."
 Physical Review X 7.2 (2017): 021002.
 
-[2] Thapa, Samudrajit, et al. "Bayesian analysis of single-particle 
-tracking data using the nested-sampling algorithm: maximum-likelihood 
-model selection applied to stochastic-diffusivity data." Physical 
+[2] Thapa, Samudrajit, et al. "Bayesian analysis of single-particle
+tracking data using the nested-sampling algorithm: maximum-likelihood
+model selection applied to stochastic-diffusivity data." Physical
 Chemistry Chemical Physics 20.46 (2018): 29018-29037.
-
 """
 
 # Import dependencies
 import numpy as np
-import matplotlib.pyplot as plt
+from yupi.stats import collect_at
+from yupi.graphics import plot_hists
 from yupi.generators import DiffDiffGenerator
-
 
 np.random.seed(0)
 
@@ -38,26 +37,25 @@ T = 1000   # Total time of the simulation
 N = 5000   # Number of trajectories
 dt = .1    # Time step
 
-# Setting different time instants
-delta_t = np.array([1, 10, 100])   # Time instants
-steps = np.int32(delta_t / dt)     # Same but in steps of the simulation
-
 # Simulating the process
 dd = DiffDiffGenerator(T, N=N, dt=dt)
 trajs = dd.generate()
 
-# Getting positions at different time intervals
-r = [[traj.r.x[step] for traj in trajs] for step in steps]
+# Setting different time instants
+time_instants = np.array([1, 10, 100])
+
+# Getting positions at different time instants
+r = [collect_at(trajs, 'rx', t, step_as_time=True) for t in time_instants]
 
 # Plotting
-for r_, delta_t_ in zip(r, delta_t):
-    plt.hist(r_, bins=30, density=True, histtype='step',
-             label=f't = {delta_t_}')
-plt.legend()
-plt.grid(True)
-plt.ylim(1e-3, 1)
-plt.xlim(-20, 20)
-plt.yscale('log')
-plt.xlabel('x')
-plt.ylabel('PDF')
-plt.show()
+plot_hists(r, bins=30, density=True,
+    labels=[f't = {dt}' for dt in time_instants],
+    xlabel='x',
+    ylabel='PDF',
+    legend=True,
+    grid=True,
+    yscale='log',
+    ylim=(1e-3, 1),
+    xlim=(-20, 20),
+    filled=True
+)
